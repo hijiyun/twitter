5a6dd8262eb55d99931109df0048d0efdb72d8be
박지윤 <tkrhkfld123@naver.com>
2024-07-16 19:21:36 +0900

    style: 로딩 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+import { auth } from "./firebase";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const init = async () => {
+    await auth.authStateReady();
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (isLoading) return <LoadingScreen />;
+
   return (
     <>
       <div>Hi</div>
