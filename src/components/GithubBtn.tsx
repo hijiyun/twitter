418b2779ce25d008b1dd8faabc292b26734fe6d9
박지윤 <tkrhkfld123@naver.com>
2024-07-17 21:05:35 +0900

    feat: 깃허브 소셜로그인 구현

--- src/components/GithubBtn.tsx
+++ src/components/GithubBtn.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 
 const S = {
   Button: styled.span`
+    margin-top: 50px;
     background-color: white;
-    font-weight: 600;
+    font-weight: 500;
+    color: black;
     padding: 10px 20px;
     border-radius: 50px;
     border: 0;
@@ -11,6 +16,7 @@ const S = {
     gap: 5px;
     align-items: center;
     justify-content: center;
+    cursor: pointer;
   `,
   Logo: styled.img`
     height: 25px;
@@ -18,10 +24,22 @@ const S = {
 };
 
 const GithubBtn = () => {
+  const navigate = useNavigate();
+
+  const onClick = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(auth, provider);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <S.Button>
+    <S.Button onClick={onClick}>
       <S.Logo src="/githubLogo.png" />
-      깃허브로 계속하기
+      깃허브로 로그인
     </S.Button>
   );
 };
