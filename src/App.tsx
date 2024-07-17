2170ad5c3a4885d0e0aa243566f3394dfbfe31fc
박지윤 <tkrhkfld123@naver.com>
2024-07-17 17:10:32 +0900

    chore: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import { auth } from "./firebase";
+import styled from "styled-components";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./components/CreateAccount";
+
+const S = {
+  Wrapper: styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+  `,
+};
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,9 +32,16 @@ function App() {
   if (isLoading) return <LoadingScreen />;
 
   return (
-    <>
-      <div>Hi</div>
-    </>
+    <S.Wrapper>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="create-account" element={<CreateAccount />} />
+      </Routes>
+    </S.Wrapper>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Profile from "./components/Profile.tsx";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <GlobalStyles />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>
 );
