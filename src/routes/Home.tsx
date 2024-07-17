661d933195c1fa9c7ddf6bf138c0491f1c23947a
박지윤 <tkrhkfld123@naver.com>
2024-07-17 19:02:46 +0900

    chore: 로그아웃 추가하기

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const logOut = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>
+        <button onClick={logOut}>로그아웃</button>
+      </h1>
+    </>
+  );
 };
 
 export default Home;
